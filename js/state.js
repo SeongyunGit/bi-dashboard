a8b5519ce216adf794b6015aa6d0c58fe1cbc07e
Seongyun Jo <okks3579@gmail.com>
2026-04-28 22:28:10 +0900

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,58 @@
+const defaultState = Object.freeze({
+  fileName: "",
+  workbook: null,
+  sheets: [],
+  selectedSheetName: "",
+  tableRows: [],
+  tableColumns: [],
+  error: null,
+  status: "idle",
+});
+
+export function createState(initialState = {}) {
+  let state = {
+    ...defaultState,
+    ...initialState,
+  };
+
+  const listeners = new Set();
+
+  const getState = () => state;
+
+  const setState = (updater) => {
+    const nextState =
+      typeof updater === "function" ? updater(state) : { ...state, ...updater };
+
+    state = nextState;
+    listeners.forEach((listener) => listener(state));
+    return state;
+  };
+
+  const resetState = () => {
+    state = {
+      ...defaultState,
+      ...initialState,
+    };
+
+    listeners.forEach((listener) => listener(state));
+    return state;
+  };
+
+  const subscribe = (listener) => {
+    listeners.add(listener);
+    listener(state);
+
+    return () => {
+      listeners.delete(listener);
+    };
+  };
+
+  return {
+    getState,
+    setState,
+    resetState,
+    subscribe,
+  };
+}
+
+export { defaultState };
